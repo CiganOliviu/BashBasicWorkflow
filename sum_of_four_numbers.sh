024e4883f3a06025fc39aba75a13d2332f72fe61
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2021-01-02 18:26:35 +0200

    sum_of_fours_numbers.sh

---
+++ sum_of_four_numbers.sh
@@ -0,0 +1,25 @@
+function get_sum_of_four_numbers {
+
+  local result=0
+
+  result=$(($1 + $2 + $3 + $4))
+
+  echo $result;
+}
+
+function get_product_of_four_numbers {
+
+  local result=1
+  
+  result=$(($1 * $2 * $3 * $4))
+
+  echo $result
+}
+
+read number1
+read number2
+read number3
+read number4
+
+get_sum_of_four_numbers $number1 $number2 $number3 $number4
+get_product_of_four_numbers $number1 $number2 $number3 $number4
